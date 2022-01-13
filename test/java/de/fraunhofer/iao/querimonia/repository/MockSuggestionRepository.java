235859d14aa8e0164ddc89b76fa2e207ea4eab30
Devrim Baran Demir <36797604+DevrimBaran@users.noreply.github.com>
2022-01-13 13:58:28 +0100

    Delete test directory

--- test/java/de/fraunhofer/iao/querimonia/repository/MockSuggestionRepository.java
+++
@@ -1,6 +0,0 @@
-package de.fraunhofer.iao.querimonia.repository;
-
-import de.fraunhofer.iao.querimonia.response.generation.ResponseSuggestion;
-
-public class MockSuggestionRepository extends MockRepository<ResponseSuggestion> implements ResponseSuggestionRepository {
-}
